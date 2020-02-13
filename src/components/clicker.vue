2380812ad7a8062d1d3f9442c8900729c1f14010
Kutasek111c <cramenpc@gmail.com>
2020-02-14 00:17:28 +0100

    add basic functionality v2

--- src/components/clicker.vue
+++ src/components/clicker.vue
@@ -5,7 +5,7 @@
         <h2>Harvest</h2>
 
 		<p>You have: {{ potatoes }} potatoes</p>
-		<p>Your employees collect 0 potatoes per second.</p>
+		<p>Your employees collect {{ employessStrength }} potatoes per second.</p>
 		<br />
 		<button v-on:click="CollectPotatoe">Collect</button>
     </div>
@@ -13,9 +13,11 @@
         <h2>Your company</h2>
 
 		<p>You have: {{ employees }} employees</p>
-		<p>One employee increases harvest by 0.1 potato per second.</p>
+		<p>One employee increases harvest by 0.1 potato per second. <br /> Each hoe improvement increases the click force by 1.</p>
 		<br />
 		<button v-on:click="HirePeople">Hire people [50 potatoes]</button>
+		<br />
+		<button v-on:click="UpgradeHoe">Upgrade your hoe! [1000 potatoes]</button>
     </div>
 	</div>
 </template>
@@ -28,6 +30,7 @@ export default {
 		potatoes: 0,
 		clickstrength: 1,
 		employees: 0,
+		employessStrength: 0 * 0.1,
     };
 	},
 	methods: {
@@ -41,9 +44,21 @@ export default {
 			}else{
 			alert("You don't have enough potatoes! Come back to the field!");
 			}
+		},
+		UpgradeHoe: function(){
+			if(this.potatoes>=1000){
+			this.clickstrenght++;
+			}else{
+			alert("You don't have enough potatoes! Come back to the field!");
+			}
+		},
+		Reload(){
+			window.setInterval(function(){
+			this.potatoes += 10;
+			}, 1000);
+		},
 		}
 	}
-  };
 </script>
 
 <style lang="scss" scoped>
@@ -55,19 +70,19 @@ height:400px;
 background-color: #FFEFD5;
 border-radius: 35px;
 width:48%;
-height:50%;
+height:60%;
 float:left;
 }
 .clicker_right{
 background-color: #FFEFD5;
 border-radius: 35px;
 width:48%;
-height:50%;
+height:60%;
 float:right;
 }
 button{
 background-color: #FFD700;
-width:200px;
+width:250px;
 height:30px;
 }
 button:hover{
